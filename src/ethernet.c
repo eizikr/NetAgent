1755e7a45b09708ea576561a51d7cf57ba79409f
Itzhak Rahamim <itzhak_r@rad.com>
2026-08-18 14:23:01 +0300

    create header struct and parse it

---
+++ src/ethernet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "netagent/ethernet.h"
+#include <string.h>
+
+int parse_ethernet(const uint8_t *buffer, size_t length, EthernetHeader *header)
+{
+    if (buffer == NULL){
+	puts("ERROR: buffer argument is NULL");
+        return ERR_BUFFER_NULL;
+    }
+
+    if (header == NULL){
+	puts("ERROR: header argument is NULL");
+        return ERR_HEADER_NULL;
+    }
+
+    if (length < 14){
+        puts("ERROR: buffer length is short");
+        return ERR_ARGS;
+    }
+
+    memcpy(header->dst_mac  , buffer, 6);
+    memcpy(header->src_mac  , buffer + 6, 6);
+    header->ethertype =
+	((uint16_t)buffer[12] << 8) |
+	buffer[13];
+
+    return ETH_OK;
+}
--- src/main.c
+++ src/main.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
+#include "netagent/ethernet.h"
+
+typedef struct {
+    uint8_t a;
+    uint32_t b;
+    uint8_t c;
+} AlignmentTest;
+
+int main(void){
+
+	EthernetHeader header;
+
+	uint8_t packet[] = {
+    		0x5c, 0x60, 0xba, 0x40, 0x7a, 0x89,
+    		0x00, 0x0c, 0x29, 0x1b, 0x7b, 0x14,
+    		0x08, 0x00
+	};
+
+	int result = parse_ethernet(
+		packet,
+		sizeof(packet),
+		&header
+	);
+
+        printf("Destination MAC:     %02x:%02x:%02x:%02x:%02x:%02x\n",
+        	header.dst_mac[0],
+            	header.dst_mac[1],
+        	header.dst_mac[2],
+        	header.dst_mac[3],
+        	header.dst_mac[4],
+        	header.dst_mac[5]);
 
 
-int main(){
+    	printf("Source MAC:          %02x:%02x:%02x:%02x:%02x:%02x\n",
+        	header.src_mac[0],
+        	header.src_mac[1],
+        	header.src_mac[2],
+        	header.src_mac[3],
+        	header.src_mac[4],
+        	header.src_mac[5]);
 
-	puts("NetAgent v0.1.0\n");
-	return 0;
+    	printf("EtherType:           0x%04x\n", header.ethertype);
+
+
+	puts("NetAgent v0.1.0");
+
+
+AlignmentTest test;
+
+printf("sizeof(AlignmentTest) = %zu\n", sizeof(AlignmentTest));
+
+printf("&test   = %p\n", (void *)&test);
+printf("&test.a = %p\n", (void *)&test.a);
+printf("&test.b = %p\n", (void *)&test.b);
+printf("&test.c = %p\n", (void *)&test.c);
+
+	return result;
 
 }
